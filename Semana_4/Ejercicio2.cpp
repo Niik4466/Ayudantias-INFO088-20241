deb69e05a33a3aae16fda7bcdd004ec991de5a48
Niik4466 <niikk4466@gmail.com>
2024-04-05 16:24:46 -0300

    Agregado Ejercicio2 para la Semana 4

---
+++ Semana_4/Ejercicio2.cpp
@@ -0,0 +1,36 @@
+/*
+Dada una Matriz dinamica A de caracteres, de tamaño MxN (M filas y N columnas) y un arreglo de 5 caracteres "palabra".
+Creen un algoritmo que: dada la Matriz de carácteres aleatorios, busque una palabra y la almacene en su primera fila (A[0])
+
+Para esto, primero deben inicializar su matriz y llenarla de carácteres aleatorios. Esto se hace utilizando código ascii y una transformación de entero a carácter
+https://elcodigoascii.com.ar
+
+Luego deben codificar la función encuentraPalabra(char** A)
+Esta función debe recorrer cada caracter del arreglo "palabra" y comprobar si existe en el arreglo,
+De ser ese el caso, va a mover ese caracter a su posición correspondiente en la primera fila de la Matriz.
+
+Para finalizar, codifiquen la función booleana encuentraPalabra, la cual recorre linealmente la primera fila del arreglo A y comprueba si
+la palabra encontrada coincide con la buscada.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+#define MIN 97  //valor mínimo que pueden tomar los carácteres
+#define MAX 123 //valor máximo que pueden tomar los carácteres
+
+#define N 5  //columnas del arreglo
+#define M 10 //filas del arreglo
+
+const char palabra[5] = {'z', 'o', 'r', 'r', 'o'}; //palabra a buscar
+
+void intercambiaMatriz(char** A, int filaA, int columnaA, int filaB, int columnaB);
+void llenaMatriz(char** A);
+void imprimeMatriz(char** A);
+void encuentraPalabra(char** A);
+bool compruebaPalabra(char** A);
+
+int main(){
+
+}
