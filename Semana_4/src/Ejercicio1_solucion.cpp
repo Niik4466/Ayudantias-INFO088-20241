6e833c2e552e1f9e05869d2a67c9d8027e0b67c4
Paskiben <94018208+Paskiben@users.noreply.github.com>
2024-04-10 08:37:20 -0400

    Update Ejercicio1_solucion.cpp
    Quitada descripción semana4/ejercicio1_solucion

--- Semana_4/src/Ejercicio1_solucion.cpp
+++ Semana_4/src/Ejercicio1_solucion.cpp
@@ -1,15 +1,3 @@
-/*
-Codifique un programa que lea un entero n positivo y mayor que 0,
-con la variable, n cree un arreglo A[0..n-1] y llenelo con datos aleatorios entre MIN y MAX
-Con una variabble m guarde el tamaño de su arreglo inicial.
-
-ahora cree una función aumentaCapacidad(int* A, int &n, const int m) que aumente la capacidad de su arreglo en m
-
-Mediante un ciclo for, siga llenando el arreglo hasta 10 veces su capacidad inicial(10*n)
-
-Desafío: probarlo con un n muy grande y medir el tiempo que lleva generar el nuevo arreglo
-*/
-
 #include <iostream>
 #include <random>
 using namespace std;
