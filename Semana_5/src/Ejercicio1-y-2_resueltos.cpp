9cff47bb57bc1b99c36db58627112974c1f28c71
Paskiben <pascasalinas12@gmail.com>
2024-04-17 19:07:32 -0400

    Soluciones semana 5

---
+++ Semana_5/src/Ejercicio1-y-2_resueltos.cpp
@@ -0,0 +1,262 @@
+#include <iostream>
+#include <cstring>
+#include <random>
+#include <vector>
+#include <array>
+using namespace std;
+
+#define N_CAR 3
+#define N_NOM 5
+#define N_APEL 10
+#define MIN_EDAD 17
+#define MAX_EDAD 49
+
+const array<char[10], N_NOM>  ellos = {"Pedro", "Juan", "Manuel", "Diego", "Agustin"};
+const array<char[10], N_NOM> ellas = {"Maria", "Claudia", "Josefina", "Paola", "Linda"};
+const array<char[10], N_APEL> apel = {"Torres", "Flores", "Campos", "Acosta", "Aguirre", "Molina", "Silva", "Rojas", "Rios", "Castillo"};
+
+struct {
+   char id;
+   char nombre[20];
+} const Carrera[N_CAR] = {{'B', "Bachillerato"},{'I', "Ingenieria"},{'M', "Magister"}};
+
+struct Alum{
+	int id;		// cree un entero alearotio positivo
+	char nombres[21];	// 2 nombres (separados por un espacio en blanco), 
+	char apellidos[21];	// 2 apellidos provenientes de apel (separados por un espacio en blanco)
+	bool masc;		// true: masculino, false: femenino
+	char idCarrera; 	// 'B', 'I' o 'M'
+	int edad;		// IN [MIN_EDAD..MAX_EDAD]
+};
+
+void genAlumnos(vector<Alum> &Alumnos);
+void print_alumnos(vector<Alum> &Alumnos);
+void quickSortId(vector<Alum> &Alumnos, const int l, const int r);
+int partitionId(vector<Alum> &Alumnos, const int l ,const int r);
+int median_aprox(vector<Alum> &Alumnos, const int l, int r);
+int binarySearchId(vector<Alum> &Alumnos, const int x);
+void insertionSortNombre(vector<Alum> &Alumnos);
+int binarySearchNombre(vector<Alum> &Alumnos, const string s);
+
+// A esta definición del main(), se le pueden pasar argumentos
+int main(int argc, char **argv) {
+	if(argc != 2){
+		cout << "Error. Debe ejecutarse como ./alum n" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+    const int n = atoi(argv[1]);
+	vector<Alum> Alumnos(n);	// ESTO ES MEMORIA DINAMICA --> no sabe el compilador cuantos bytes son
+
+	genAlumnos(Alumnos);
+    fputs_unlocked("Arreglo inicial:\n",stdout);
+	print_alumnos(Alumnos);
+    fputs_unlocked("\nOrdenando arreglo por id\n", stdout);
+    quickSortId(Alumnos, 0, n-1);
+    fputs_unlocked("arreglo ordenado!, imprimiendo arreglo:\n", stdout);
+    print_alumnos(Alumnos);
+
+    const int x = rand();
+    printf("\nbuscando id: %d\n", x);
+    int pos = binarySearchId(Alumnos, x);
+    if (pos == -1)
+        fputs_unlocked("id no encontrado!\n", stdout);
+    else{
+        printf("la id fue encontrada en la posicion: %d\n", pos);
+    }
+
+    fputs_unlocked("\nOrdenando arreglo por nombre\n", stdout);
+    insertionSortNombre(Alumnos);
+    fputs_unlocked("arreglo ordenado!, imprimiendo arreglo:\n", stdout);
+    print_alumnos(Alumnos);
+
+    string s;
+    if(rand()%2 == 0){
+			s = string(ellas.at(rand()%8));
+			s += " ";
+			s += ellas.at(rand()%8);
+	}else{
+			s = string(ellos.at(rand()%8));
+			s += " ";
+			s += ellos.at(rand()%8);
+	}
+    printf("\nbuscando nombre: %s\n", s.c_str());
+    pos = binarySearchNombre(Alumnos, s);
+    if (pos == -1)
+        fputs_unlocked("\nid no encontrado!\n", stdout);
+    else{
+        printf("la id fue encontrada en la posicion: %d\n", pos);
+    }
+
+	return EXIT_SUCCESS;
+}
+
+// generar n alumnos con dos nombres y 2 apellidos aleatorios desde los arreglos globales
+void genAlumnos(vector<Alum> &Alumnos) {
+	int i, a, b;
+
+	for(i=0; i < (int)Alumnos.size(); ++i){
+		Alumnos.at(i).id = rand();
+		strcpy(Alumnos.at(i).apellidos, apel.at(rand()%N_APEL)); // primer apellido
+		strcat(Alumnos.at(i).apellidos, " ");
+		strcat(Alumnos.at(i).apellidos, apel.at(rand()%N_APEL));
+
+        a = b = rand()%N_NOM;
+            while (a == b)
+				b = rand()%N_NOM;
+		
+		if(rand()%2 == 0){
+			Alumnos.at(i).masc = false;
+            
+			strcpy(Alumnos.at(i).nombres, ellas.at(a));
+			strcat(Alumnos.at(i).nombres, " ");
+			strcat(Alumnos.at(i).nombres, ellas.at(b));
+		}else{
+			Alumnos.at(i).masc = true;
+            
+			strcpy(Alumnos.at(i).nombres, ellos.at(a));
+			strcat(Alumnos.at(i).nombres, " ");
+			strcat(Alumnos.at(i).nombres, ellos.at(b));
+		}
+
+		Alumnos.at(i).edad = MIN_EDAD + rand()%(MAX_EDAD-MIN_EDAD+1);
+		Alumnos.at(i).idCarrera = Carrera[rand()%N_CAR].id;
+	}
+}
+
+void print_alumnos(vector<Alum> &Alumnos) {
+	for (Alum &alumno : Alumnos){
+		printf("Id        : %d\n", alumno.id);
+
+		if (alumno.masc)
+			fputs_unlocked("Sexo      : Masculino \n", stdout);
+		else
+			fputs_unlocked("Sexo      : Femenino \n", stdout);
+
+		printf("Nombres   : %s\n", alumno.nombres);
+		printf("Apellidos : %s\n", alumno.apellidos);
+		fputs_unlocked("Carreara  : ",stdout);
+		switch (alumno.idCarrera) {
+			case 'B':
+				printf("%s\n",Carrera[0].nombre);
+				break;
+			case 'I':
+				printf("%s\n",Carrera[1].nombre);
+				break;
+			case 'M':
+				printf("%s\n",Carrera[2].nombre);
+				break;
+			default:
+				fputs_unlocked(" NO DEFINIDA !! \n", stdout);
+				break;
+		}
+		printf("Edad      : %d\n\n", alumno.edad);
+	}
+}
+
+void quickSortId(vector<Alum> &Alumnos, const int l, const int r) {
+    if(l>=r)
+        return;
+    
+    int p = partitionId(Alumnos, l, r);
+    quickSortId(Alumnos, p+1, r);
+    quickSortId(Alumnos, l, p-1);
+}
+
+int partitionId(vector<Alum> &Alumnos, const int l ,const int r) {
+    const int pivot = median_aprox(Alumnos, l, r);
+    //toma la posiccion a la derecha del pivote
+    //que inicia en el mayor indice del intervalo
+    int rp= r+1;
+
+    for(int j=r; j>l; --j){
+        //si el valor de la posicion actual es mayor al pivote, la rp se mueve un espacio a la izquierda
+        // (pues ahora hay un elemento mas que queda a la derecha del pivote)
+        if(Alumnos.at(j).id > pivot){
+            --rp;
+            swap(Alumnos.at(rp), Alumnos.at(j));
+        }
+    }
+    //dejamos el pivote en la posicion correcta
+    swap(Alumnos.at(rp-1), Alumnos.at(l));
+    return rp-1;
+}
+
+//calcula la mediana aproximada del intervalo
+int median_aprox(vector<Alum> &Alumnos, const int l, int r) {
+    int m = l + (r-l)/2;
+    Alum tmp;
+
+    //Calculamos la media aproximada que vimos en ejercicios previos
+    if (Alumnos.at(l).id <= Alumnos.at(m).id){
+        if (Alumnos.at(r).id <= Alumnos.at(l).id) return Alumnos.at(l).id;  /* r l m */
+        if (Alumnos.at(m).id <= Alumnos.at(r).id) r = m;                    /* l m r */
+                                                                            /* l r m */
+    } else{
+        if (Alumnos.at(l).id <= Alumnos.at(r).id) return Alumnos.at(l).id;  /* m l r */
+        if (Alumnos.at(r).id <= Alumnos.at(m).id) r = m;                    /* r m l */
+                                                                            /* m r l */
+    }
+
+    //colocamos el pivote al inicio del intervalo en preparacion para la particion
+    tmp = Alumnos.at(r);
+    Alumnos.at(r) = Alumnos.at(l);
+    Alumnos.at(l) = tmp;
+    //por el paso anterior el valor de la media es la id de Alumnos.at(l)
+    return Alumnos.at(l).id;
+}
+
+int binarySearchId(vector<Alum> &Alumnos, const int x) {
+    int l = 0, r = Alumnos.size()-1;
+
+    while (l <= r) {
+        int m = l + (r-l)/2;
+
+        if (Alumnos.at(m).id == x)
+            return m;
+
+        //si x es mayor, ignoramos la mitad izquierda
+        if (Alumnos.at(m).id < x)
+            l = m+1;
+        //si x es menor, ignoramos la mitad derecha
+        else
+            r = m-1;
+    }
+
+    //retornamos -1 si no se encontro
+    return -1;
+}
+
+void insertionSortNombre(vector<Alum> &Alumnos) {
+    int j;
+    Alum key;
+    
+    for (int i=1; i < (int)Alumnos.size(); ++i) {
+        key = Alumnos.at(i);
+        j = i-1;
+
+        while (j>=0 && string(Alumnos.at(j).nombres) > string(key.nombres)) {
+            Alumnos.at(j+1) = Alumnos.at(j);
+            j = j-1;
+        }
+        Alumnos.at(j+1) = key;
+    }
+}
+
+int binarySearchNombre(vector<Alum> &Alumnos, const string s) {
+    int l = 0, r = Alumnos.size()-1;
+
+    while (l<=r) {
+        int m = l + (r-l)/2;
+
+        if (string(Alumnos.at(m).nombres) == s)
+            return m;
+
+        if (s > string(Alumnos.at(m).nombres))
+            l = m+1;
+        else
+            r = m-1;
+    }
+
+    return -1;
+}
---
+++ Semana_5/src/Ejercicio3_resueltos.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <random>
+using namespace std;
+
+int quickSelect(vector<int> &A, int l, int r, const int k);
+int partition(vector<int> &A, int l, int r);
+int median_aprox(vector<int> &A, const int l, int r);
+void print_vec(vector<int> &A);
+void quickSort(vector<int> &A, const int l, const int r);
+
+#define MIN 0
+#define MAX 100
+
+int main(int argc, char **argv){
+    if (argc != 3){
+        cout << "Error. debe ejecutarse como ./prog n k" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    const int n = atoi(argv[1]), k = atoi(argv[2]);
+    if (n < 1 || k < 1){
+        cout << "n debe ser mayor que 0!!\n";
+        exit(EXIT_FAILURE);
+    } 
+
+    vector<int> A(n);
+    //llenamos el vector utilizando iteradores (ver más aquí -> https://learn.microsoft.com/es-es/cpp/standard-library/iterators?view=msvc-170)
+    for (int &iterador: A)
+        iterador = MIN + rand()%(MAX-MIN);
+
+    printf("El vector es:\n");
+    print_vec(A);
+    
+    printf("\nEl %d elemento menor es: %d\n", k, quickSelect(A, 0, n-1, k-1));
+    
+    fputs_unlocked("arreglo ordenado para facil comprobacion:\n",stdout);
+    quickSort(A, 0, n-1);
+    print_vec(A);
+    
+
+    return EXIT_SUCCESS;
+}
+
+int quickSelect(vector<int> &A, int l, const int r, const int k) {
+    //partition deja el 'p' elemento menor en su lugar en la lista
+    int p = partition(A, l, r);
+    
+    // si k = p significa que se encontro el k elemento menor ordenado en la lista
+    if(k == p) return A[p];
+
+    // si k < p significa que partition encontró un p elemento mayor al buscado,
+    //por lo que nos quedamos con los elementos a la izquierda (menores a p)
+    if(k < p)
+        return quickSelect(A, l, p-1, k); 
+    //si no, significa que partition ordenó un elemento p más pequeño al buscado
+    //por lo que nos quedamos con los elementos a la derecha (mayores a p)
+    return quickSelect(A, p+1, r, k);
+}
+
+//partition, median_aproxy quickSort son adaptaciones de los usados en el anterior ejercicio
+int partition(vector<int> &A, const int l ,const int r) {
+    const int pivot = median_aprox(A, l, r);
+    int rp= r+1;
+
+    for(int j=r;j>l;--j){
+        if(A.at(j) > pivot){
+            --rp;
+            swap(A.at(rp), A.at(j));
+        }
+    }
+    swap(A.at(rp-1), A.at(l));
+    return rp-1;
+}
+
+int median_aprox(vector<int> &A, const int l, int r) {
+    int m = l + (r-l)/2;
+    int tmp;
+
+    if (A.at(l) <= A.at(m)){
+        if (A.at(r) <= A.at(l)) return A.at(l); /* r l m */
+        if (A.at(m) <= A.at(r)) r = m;          /* l m r */
+                                                /* l r m */
+    } else{
+        if (A.at(l) <= A.at(r)) return A.at(l); /* m l r */
+        if (A.at(r) <= A.at(m)) r = m;          /* r m l */
+                                                /* m r l */
+    }
+    tmp = A.at(r);
+    A.at(r) = A.at(l);
+    A.at(l) = tmp;
+
+    return A.at(l);
+}
+
+//recorremos el vector con un iterador y imprimimos cada elemento
+void print_vec(vector<int> &A) {
+    for (int num: A)
+        printf("%d ", num);
+    fputc_unlocked('\n', stdout);
+}
+
+void quickSort(vector<int> &A, const int l, const int r) {
+    if(l>=r)
+        return;
+    
+    int p = partition(A, l, r);
+    quickSort(A, p+1, r);
+    quickSort(A, l, p-1);
+}
