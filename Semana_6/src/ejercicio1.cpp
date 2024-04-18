9beac1a03a519ebe57614c94ca9b0fd7c6c28fb0
Paskiben <pascasalinas12@gmail.com>
2024-04-18 19:18:17 -0400

    Subidos ejercicios 1, 2 y 3 de la semana 6

---
+++ Semana_6/src/ejercicio1.cpp
@@ -0,0 +1,126 @@
+/*
+Ejercicio 1
+Dada una lista ordenada ascendentemente con la función inserInList(nodo **l, int num) hecha en clase,
+inviertala mediante la función reverseList(nodo **l).
+*/
+
+#include <random>
+
+using namespace std;
+
+struct nodeList {
+	int val;
+	nodeList* next;
+};
+typedef struct nodeList nodo;
+
+# define MAX 50
+
+void appendToListL(nodo **l, int num);
+void appendToListR(nodo **l, int num);
+void inserInList(nodo **l, int num);
+bool removeFromList(nodo **l, int num);
+void printList(nodo *l);
+void reverseList(nodo **l, nodo *reversel);
+
+int main(int argc, char **argv){
+	int i, x;
+	nodo *miLista = NULL;
+
+	for (i=1; i<=10; i++){
+		x = rand()%MAX;
+		inserInList(&miLista, x);	
+	}
+	printList(miLista);
+
+
+
+	return EXIT_SUCCESS;
+}
+
+void reverseList(nodo **l) {
+    return;
+}
+
+// elimina el primer nodo de la lista con valor num que encuentra 
+// (de izquierda a derecha)
+bool removeFromList(nodo **l, int num){
+	if (*l == nullptr)
+		return false;
+
+	nodo *p = *l;
+	if (p->val == num){
+		*l = p->next;
+		delete p;
+		return true;
+	}
+
+	while(p->next != nullptr && p->next->val != num)
+		p = p->next;
+
+	if (p->next != nullptr){
+		nodo *q = p->next;		// Este es el nodo que se desea eliminar
+		p->next = q->next;
+		delete q;
+		return true;
+	}
+	
+	return false;
+}
+
+// aniade un nodo al inicio de la lista
+void appendToListL(nodo **l, int num){
+	nodo *nuevo = new nodo;
+	nuevo->val = num;
+	nuevo->next = *l;
+	*l = nuevo;
+}
+
+// aniade un nodo al final de la lista
+void appendToListR(nodo **l, int num){
+	nodo *nuevo = new nodo;
+	nuevo->val = num;
+	nuevo->next = NULL;
+
+	printf("Aniadiendo el nodo con val = %d\n", num);
+	if (*l == nullptr)
+		*l = nuevo;
+	else{
+		nodo *p = *l;
+		for(;p->next != nullptr; p = p->next);
+
+		p->next = nuevo;
+	}
+}
+
+// inserta el nodo en su posicion correcta (ascendente)
+void inserInList(nodo **l, int num){
+	nodo *nuevo = new nodo;
+	nuevo->val = num;
+	nuevo->next = NULL;
+
+	if (*l == nullptr)
+		*l = nuevo;
+	else{
+		nodo *p = *l;
+		if (num <= p->val){
+			// se inserta al inicio...
+			nuevo->next = p;
+			*l = nuevo;
+		}else{
+			for(;p->next != nullptr && p->next->val < num; p=p->next);
+
+			nuevo->next = p->next; // nuevo se inserta justo antes de p->next
+			p->next = nuevo;
+		}
+	}
+}
+
+void printList(nodo *l){
+	nodo *p = l;
+	fputs_unlocked("Lista = ", stdout);
+	for(;p != nullptr; p=p->next){
+		printf("%d ",p->val);
+	}
+	fputs_unlocked("\n", stdout);
+}
---
+++ Semana_6/src/ejercicio2-y-3.cpp
@@ -0,0 +1,129 @@
+/*
+Ejercicio 2
+Dada una lista enlazada ordenada ascendentemente con n elementos,
+separe los elementos de la lista que sean pares en una lista e impares en otra en la funcion separa_pares_impares(nodo **l, nodo *pares, nodo *impares).
+
+Ejercicio 3
+Teniendo las listas de pares e impares del ejercicio 2
+ahora debe unirlas en una nueva lista manteniendo el orden original en la funcion juntar_listas(nodo **pares, nodo **impares, nodo *l2).
+*/
+
+#include <random>
+
+using namespace std;
+
+struct nodeList {
+	int val;
+	nodeList* next;
+};
+typedef struct nodeList nodo;
+
+# define MAX 50
+
+void appendToListL(nodo **l, int num);
+void appendToListR(nodo **l, int num);
+void inserInList(nodo **l, int num);
+bool removeFromList(nodo **l, int num);
+void printList(nodo *l);
+void separa_pares_impares(nodo **l, nodo *pares, nodo *impares);
+
+int main(int argc, char **argv){
+	int i, x;
+	nodo *miLista, *pares, *impares, *miLista2;
+    miLista = pares = impares = miLista2 = NULL;
+
+	for (i=1; i<=10; i++){
+		x = rand()%MAX;
+		inserInList(&miLista, x);	
+	}
+	printList(miLista);
+
+
+
+	return EXIT_SUCCESS;
+}
+
+void separa_pares_impares(nodo **l, nodo *pares, nodo *impares) {
+    return;
+}
+
+void juntar_listas(nodo **pares, nodo **impares, nodo *l2) {
+    return;
+}
+
+bool removeFromList(nodo **l, int num){
+	if (*l == nullptr)
+		return false;
+
+	nodo *p = *l;
+	if (p->val == num){
+		*l = p->next;
+		delete p;
+		return true;
+	}
+
+	while(p->next != nullptr && p->next->val != num)
+		p = p->next;
+
+	if (p->next != nullptr){
+		nodo *q = p->next;
+		p->next = q->next;
+		delete q;
+		return true;
+	}
+	
+	return false;
+}
+
+void appendToListL(nodo **l, int num){
+	nodo *nuevo = new nodo;
+	nuevo->val = num;
+	nuevo->next = *l;
+	*l = nuevo;
+}
+
+void appendToListR(nodo **l, int num){
+	nodo *nuevo = new nodo;
+	nuevo->val = num;
+	nuevo->next = NULL;
+
+	printf("Aniadiendo el nodo con val = %d\n", num);
+	if (*l == nullptr)
+		*l = nuevo;
+	else{
+		nodo *p = *l;
+		for(;p->next != nullptr; p = p->next);
+
+		p->next = nuevo;
+	}
+}
+
+void inserInList(nodo **l, int num){
+	nodo *nuevo = new nodo;
+	nuevo->val = num;
+	nuevo->next = NULL;
+
+	if (*l == nullptr)
+		*l = nuevo;
+	else{
+		nodo *p = *l;
+		if (num <= p->val){
+			nuevo->next = p;
+			*l = nuevo;
+		}else{
+			for(;p->next != nullptr && p->next->val < num; p=p->next);
+
+			nuevo->next = p->next;
+			p->next = nuevo;
+		}
+	}
+}
+
+void printList(nodo *l){
+	nodo *p = l;
+	fputs_unlocked("Lista = ", stdout);
+	for(;p != nullptr; p=p->next){
+		printf("%d ",p->val);
+	}
+	fputs_unlocked("\n", stdout);
+}
